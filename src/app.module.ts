98e975af1075fe21b1e18f6906a6bb8bfe36af93
Gabriel Aquino <gabriel.aquino@contraclabs.tech>
2024-06-04 21:32:32 -0300

    feat: first commit

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { LogsModule } from './modules/logs/logs.module';
+
+@Module({
+  imports: [LogsModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
